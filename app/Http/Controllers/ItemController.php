18ccb630a68b3f1cdd4aa8c4845432af3e46030b
John L. Sims <john@ejincollective.com>
2020-01-30 16:54:56 +0000

    Item CM Added

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Item;
+
+class ItemController extends Controller
+{ 
+  public function createItem(Request $request, Item $item){
+    $newItem = Item::create([
+      'item' => $request->item,
+    ]);
+
+/*
+    //Associate With User
+    $user = auth()->guard('api')->user();
+    $newAddress->user()->associate($user->id);
+    $newAddress->save();
+*/
+
+    return $newItem;
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
 Route::get('/user', 'AuthController@user');
 Route::post('/logout', 'AuthController@logout');
+
+Route::post('/listing', 'ItemController@createItem')->middleware('auth:api');
